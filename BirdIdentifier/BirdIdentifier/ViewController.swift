b25a4ba36da48763e65b0cd77c48c6e36ba7126e
Trupti Veer <41720688+trupti-veer@users.noreply.github.com>
2022-05-17 16:13:55 +0530

    Add files via upload

---
+++ BirdIdentifier/BirdIdentifier/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BirdIdentifier
+//
+//  Created by Trupti Veer on 11/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
